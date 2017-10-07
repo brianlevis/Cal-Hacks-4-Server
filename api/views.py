633f01998cfa76666442624122b0423746aa2797
brianlevis <brianlevis1@gmail.com>
2017-10-07 13:42:16 -0700

    add skeleton for listener

--- api/views.py
+++ api/views.py
@@ -3,54 +3,37 @@ import subprocess, sys
 
 listeners = {}
 
-class Tracker:
-    def __init__(self, region_name, lat, lon):
-        self.region_name = region_name
-        self.lat = lat
-        self.lon = lon
-
 def get_tweets(request):
     print("Get event: " + str(request))
-    if request.method != "GET":
+    if request.method != "GET" or ('region' not in request.GET) or ('event' not in request.GET):
         return HttpResponseBadRequest("Request should be a GET request with fields region and event.")
     return JsonResponse({'hell yeah?': 'HELL YEAH!!!'})
 
 def get_events(request):
     print("Get data: " + str(request))
-    if request.method != "GET":
+    if request.method != "GET" or ('event' not in request.GET):
         return HttpResponseBadRequest("Request should be a GET request with field region.")
     return JsonResponse({'hell yeah?': 'HELL YEAH!!!'})
 
-
 def add_tracker(request):
     print("Add Tracker: " + str(request))
-    if request.method != "GET":
+    if request.method != "GET" or 'region' not in request.GET:
         return HttpResponseBadRequest("Request should be a GET request with fields region, latitude, and longitude.")
-    try:
-        region_name = request.GET['region']
-        if region_name in listeners:
-            HttpResponseBadRequest("Region " + region_name + " already exists.")
-        lat, lon = request.GET['latitude'], request.GET['longitude']
-        p = subprocess.Popen([sys.executable, 'listener.py'],
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT)
-        listeners[region_name] = p
-    except KeyError:
-        return HttpResponseBadRequest("Request should be a GET request with fields region, latitude, and longitude.")
+
+    region = request.GET['region']
+    if region in listeners:
+        HttpResponseBadRequest("Region " + region + " already exists.")
+
+    latitude, longitude = request.GET['latitude'], request.GET['longitude']
+    p = subprocess.Popen(['listener.py', '--region', region, '--latitude', latitude, '--longitude', longitude],
+                         stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.STDOUT)
+    listeners[region] = p
 
 
 def remove_tracker(request):
     print("Remove Tracker: " + str(request))
     if request.method != "GET":
         return HttpResponseBadRequest("Request should be a GET request with fields region_name.")
-    try:
-        region_name = request.GET['region']
-        if region_name in listeners:
-            HttpResponseBadRequest("Region " + region_name + " already exists.")
-        lat, lon = request.GET['latitude'], request.GET['longitude']
-        p = subprocess.Popen([sys.executable, 'listener.py'],
-                             stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT)
-        locations[region_name] = p
-    except KeyError:
-        return HttpResponseBadRequest("Request should be a GET request with fields region, latitude, and longitude.")
+    return JsonResponse({'hell yeah?': 'HELL YEAH!!!'})
