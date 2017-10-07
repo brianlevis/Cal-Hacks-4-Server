5ef11f4da3022de0f76868a0994d6f27b480ffed
brianlevis <brianlevis1@gmail.com>
2017-10-07 16:47:38 -0700

    sample events

--- api/views.py
+++ api/views.py
@@ -6,24 +6,41 @@ listeners = {}
 LISTENER_SCRIPT = 'listener/listener.py'
 
 
-def get_tweets(request):
-    print("Get event: " + str(request))
-    if request.method != "GET" or ('region' not in request.GET) or ('event' not in request.GET):
-        return HttpResponseBadRequest("Request should be a GET request with fields region and event.")
-    return JsonResponse({'hell yeah?': 'HELL YEAH!!!'})
-
-
 def get_events(request):
     print("Get data: " + str(request))
-    if request.method != "GET" or ('event' not in request.GET):
-        return HttpResponseBadRequest("Request should be a GET request with field region.")
+    if request.method != "GET" or ('trackerId' not in request.GET):
+        return HttpResponseBadRequest("Request should be a GET request with field trackerId.")
+    response = {
+        'ClownCon 2017': {
+            'name': 'ClownCon 2017',
+            'latitude': 37.869407,
+            'longitude': -122.266937,
+        },
+        'Cal Hacks 4.0': {
+            'name': 'Cal Hacks 4.0',
+            'latitude': 37.870642,
+            'longitude': -122.251471,
+        },
+        'Water Polo Championship': {
+            'name': 'Water Polo Championship',
+            'latitude': 37.868723,
+            'longitude': -122.262037,
+        },
+    }
+    return JsonResponse(response)
+
+
+def get_tweets(request):
+    print("Get event: " + str(request))
+    if request.method != "GET" or ('trackerId' not in request.GET) or ('trackerId' not in request.GET):
+        return HttpResponseBadRequest("Request should be a GET request with field trackerId.")
     return JsonResponse({'hell yeah?': 'HELL YEAH!!!'})
 
 
 def add_tracker(request):
     print("Add Tracker: " + str(request))
-    if request.method != "GET" or 'region' not in request.GET:
-        return HttpResponseBadRequest("Request should be a GET request with fields region, latitude, and longitude.")
+    if request.method != "GET" or 'id' not in request.GET:
+        return HttpResponseBadRequest("Request should be a GET request with fields id, latitude, and longitude.")
 
     region = request.GET['region']
     if region in listeners:
