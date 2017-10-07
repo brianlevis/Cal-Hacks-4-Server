2be8f9bed933fe2c1a48e0af7936bb936df35b0d
brianlevis <brianlevis1@gmail.com>
2017-10-07 12:55:40 -0700

    update endpoints

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^getData', views.get_data, name='getData'),
+    url(r'^getEvents', views.get_events, name='getEvents'),
+    url(r'^getTweets', views.get_tweets, name='getTweets'),
     url(r'^addTracker', views.add_tracker, name='addTracker'),
     url(r'^removeTracker', views.remove_tracker, name='removeTracker'),
 ]
--- api/views.py
+++ api/views.py
@@ -9,28 +9,34 @@ class Tracker:
         self.lat = lat
         self.lon = lon
 
-def get_data(request):
+def get_tweets(request):
+    print("Get event: " + str(request))
+    if request.method != "GET":
+        return HttpResponseBadRequest("Request should be a GET request with fields region and event.")
+    return JsonResponse({'hell yeah?': 'HELL YEAH!!!'})
+
+def get_events(request):
     print("Get data: " + str(request))
     if request.method != "GET":
-        return HttpResponseBadRequest("Request should be a GET request with field region_name.")
+        return HttpResponseBadRequest("Request should be a GET request with field region.")
     return JsonResponse({'hell yeah?': 'HELL YEAH!!!'})
 
 
 def add_tracker(request):
     print("Add Tracker: " + str(request))
     if request.method != "GET":
-        return HttpResponseBadRequest("Request should be a GET request with fields region_name, latitude, and longitude.")
+        return HttpResponseBadRequest("Request should be a GET request with fields region, latitude, and longitude.")
     try:
         region_name = request.GET['region']
         if region_name in locations:
             HttpResponseBadRequest("Region " + region_name + " already exists.")
-        lat, lon = request.GET['latitute'], request.GET['longitude']
+        lat, lon = request.GET['latitude'], request.GET['longitude']
         p = subprocess.Popen([sys.executable, 'tracker.py'],
                              stdout=subprocess.PIPE,
                              stderr=subprocess.STDOUT)
         locations[region_name] = p
     except KeyError:
-        return HttpResponseBadRequest()
+        return HttpResponseBadRequest("Request should be a GET request with fields region, latitude, and longitude.")
 
 
 def remove_tracker(request):
@@ -41,10 +47,10 @@ def remove_tracker(request):
         region_name = request.GET['region']
         if region_name in locations:
             HttpResponseBadRequest("Region " + region_name + " already exists.")
-        lat, lon = request.GET['latitute'], request.GET['longitude']
+        lat, lon = request.GET['latitude'], request.GET['longitude']
         p = subprocess.Popen([sys.executable, 'tracker.py'],
                              stdout=subprocess.PIPE,
                              stderr=subprocess.STDOUT)
         locations[region_name] = p
     except KeyError:
-        return HttpResponseBadRequest()
+        return HttpResponseBadRequest("Request should be a GET request with fields region, latitude, and longitude.")
