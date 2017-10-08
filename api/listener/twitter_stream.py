4c669a346a38eb0634752f5ab24f514cf8461956
Tejas Priyadarshan <tejaspriyadarshan@berkeley.edu>
2017-10-07 18:14:26 -0700

    finished twitter_stream.py first implementation

--- api/listener/twitter_stream.py
+++ api/listener/twitter_stream.py
@@ -7,5 +7,20 @@ keywords: list-like of strings
 return a list-like of tweets
 
 """
-def get_filtered_tweets(latitude, longitude, range, keywords):
-    pass
+def realtime_stream_by_city(cityBoxCoordinates, keywords=[]):
+    #function that collects all tweets from a city/keyword set within a 24 hour time period.
+    locationalTweets = twitAPI.request('statuses/filter', {'locations':cityBoxCoordinates , 'track': keywords })
+    return locationalTweets.get_iterator()
+
+
+def geoSearch_valid_locations(latitude, longitude, distance):
+    #function that creates a json formatted object of all the possible locations closest to you in your radius
+    distance = str(distance*5280) + "ft"
+    places = twitAPI.request("geo/search", {"long": longitude, "lat": latitude, "accuracy": distance, "granularity": "city"})
+    return places.json()
+
+
+def get_filtered_tweets_by_location(latitude, longitude, distance, keywords=[]):
+    #returns a list of tweet objects by location and distance of your setting
+    boxSetCoordinates = geoSearch_valid_locations(latitude, longitude, distance)['result']['places'][0]['bounding_box']['coordinates'][0]
+    return realtime_stream_by_city(boxSetCoordinates, keywords)
