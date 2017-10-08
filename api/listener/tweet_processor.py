df09e7ef5fca63e081d52f4ec15cc409f13f203a
Andrew Chan <andrewkchan@berkeley.edu>
2017-10-07 21:18:41 -0700

    test

--- api/listener/tweet_processor.py
+++ api/listener/tweet_processor.py
@@ -1,7 +1,19 @@
+from TwitterAPI import TwitterAPI
+#import keystore
+#import httplib, urllib
+import json
+from twitter_stream import get_filtered_tweets_by_location
+
+consumerKey = "YOBQnrulcSHFtEiJnnWiDaVhG"
+consumerSecret = "zQRNJ2J8hw1kccQO1Sb3X5bhbiVAFph6KSJr46d6cbZqP4rTZR"
+tokenKey = "814761966723960832-EOKXXImj9OA8sNz51dasntrhbeReSsG"
+tokenSecret = "CoKgSROWgP1Jxbql5oEtHnzt3Tt9jDJ7SAMfmJdsAOiJL"
+twitAPI = TwitterAPI(consumerKey,consumerSecret, tokenKey, tokenSecret)
+
 # In the future, import list of keyword sets from listener.py- for now, just a list of our keywords
-KEYWORD_SETS = [
-    ('protest', 'rally', 'controversial speaker'),
-    ('game', 'football'),
+KEYWORD_LISTS = [
+    ['protest', 'rally', 'controversial speaker'],
+    ['game', 'football']
 ]
 
 """
@@ -12,9 +24,38 @@ return a list-like of hashtags
 
 """
 
+def groupTweets():
+    classify = {}
+    for key in KEYWORD_LISTS:
+        tweets = get_filtered_tweets_by_location(37.7749, 122.4194, 10, key)
+        for t in tweets:
+            if t['entities']['hashtags'] != None:
+                for hashtag in t['entities']['hashtags']:
+                    if classify[hashtag['text']] == None:
+                        classify[hashtag['text']] = [t]
+                    else:
+                        classify[hashtag['text']].append(t)
+    return classify
+test = groupTweets()
+print(test)
 
-def extract_hashtags(tweets, keyword_sets):
-    pass
+#key is dependent on whether there's a hashtag or not
+# def extract_hashtags(tweets, keyword_lists):
+#     hashtag_dict = {}
+#     for t in tweets:
+#         if t['entities']['hashtags'] != []:
+#             for hashtag in t['entities']['hashtags']:
+#                 if hashtag_dict[hashtag['text']] == None:
+#                     hashtag_dict[hashtag['text']] = [t]
+#                 else:
+#                     hashtag_dict[hashtag['text']].append(t)
+#     return hashtag_dict
+
+
+    # for keylist in KEYWORD_SETS:
+    #     #keywordTweets = twitAPI.request('statuses/filter', {'track': keyword_sets})
+    #     print(keylist)
+#extract_hashtags("", KEYWORD_SETS)
 
 
 """
@@ -44,3 +85,37 @@ return a tuple of (positivity/sentiment, controversy)
 
 def get_sentiment(tweets):
     pass
+# Tracker that looks at location of the
+# user while also filtering tweets by those and key locations
+
+
+
+#function that collects all tweets from a city within a 24 hour time period.
+def tweetStreamCity(cityBoxCoordinates):
+    locationalTweets = twitAPI.request('statuses/filter', {'locations':cityBoxCoordinates})
+    return locationalTweets
+
+#function that collects all tweets filtering by keywords
+
+def tweetStreamKeywords(listOfKeyWords):
+    keywordTweets = twitAPI.request('statuses/filter', {'track':listOfKeyWords})
+    return keywordTweets
+
+#function that prints out each tweet inside of the list of tweets
+def printOutTweets(twitterTweetsList):
+    for tweet in twitterTweetsList.get_iterator():
+        print(tweet['text'])
+        if tweet['entities']['hashtags'] != []:
+            print('#'+tweet['entities']['hashtags'][0]['text'])
+
+#applies a function to each tweet inside of a list of tweets
+def funcApplyTweet(twitterTweetsList, func):
+    for tweet in twitterTweetsList.get_iterator():
+        func(tweet)
+
+
+# keywordList = ['lebron']
+#
+# rv = tweetStreamKeywords(keywordList)
+#
+# printOutTweets(rv)
--- api/listener/twitter_stream.py
+++ api/listener/twitter_stream.py
@@ -1,3 +1,9 @@
+from TwitterAPI import TwitterAPI
+
+KEYWORD_LISTS = [
+    ['protest', 'rally', 'controversial speaker'],
+    ['game', 'football']
+]
 """
 
 latitude, longitude: floats
@@ -7,10 +13,15 @@ keywords: list-like of strings
 return a list-like of tweets
 
 """
+consumerKey = "YOBQnrulcSHFtEiJnnWiDaVhG"
+consumerSecret = "zQRNJ2J8hw1kccQO1Sb3X5bhbiVAFph6KSJr46d6cbZqP4rTZR"
+tokenKey = "814761966723960832-EOKXXImj9OA8sNz51dasntrhbeReSsG"
+tokenSecret = "CoKgSROWgP1Jxbql5oEtHnzt3Tt9jDJ7SAMfmJdsAOiJL"
+twitAPI = TwitterAPI(consumerKey,consumerSecret, tokenKey, tokenSecret)
 def realtime_stream_by_city(cityBoxCoordinates, keywords=[]):
     #function that collects all tweets from a city/keyword set within a 24 hour time period.
     locationalTweets = twitAPI.request('statuses/filter', {'locations':cityBoxCoordinates , 'track': keywords })
-    return locationalTweets.get_iterator()
+    return locationalTweets
 
 
 def geoSearch_valid_locations(latitude, longitude, distance):
@@ -24,3 +35,19 @@ def get_filtered_tweets_by_location(latitude, longitude, distance, keywords=[]):
     #returns a list of tweet objects by location and distance of your setting
     boxSetCoordinates = geoSearch_valid_locations(latitude, longitude, distance)['result']['places'][0]['bounding_box']['coordinates'][0]
     return realtime_stream_by_city(boxSetCoordinates, keywords)
+
+
+def groupTweets():
+    classify = {}
+    for key in KEYWORD_LISTS:
+        tweets = get_filtered_tweets_by_location(37.7749, -122.4194, 10, key)
+        for t in tweets:
+            if t['entities']['hashtags'] != None:
+                for hashtag in t['entities']['hashtags']:
+                    if classify[hashtag['text']] == None:
+                        classify[hashtag['text']] = [t]
+                    else:
+                        classify[hashtag['text']].append(t)
+    return classify
+test = groupTweets()
+print(test)
